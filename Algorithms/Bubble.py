053081dc34d99aec0cc62853e2f7afbfd8487037
Welemhret Baraki <weldat.baraki@gmail.com>
2023-07-12 01:33:40 +0200

    Create Bubble.py

---
+++ Algorithms/Bubble.py
@@ -0,0 +1,9 @@
+def bubblesort(list):
+
+# Swap the elements to arrange in order
+   for iter_num in range(len(list)-1,0,-1):
+      for idx in range(iter_num):
+         if list[idx]>list[idx+1]:
+            temp = list[idx]
+            list[idx] = list[idx+1]
+            list[idx+1] = temp
